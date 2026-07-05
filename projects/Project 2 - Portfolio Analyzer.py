1dbb570c6756bd71f44e366fcea033f33e5ad6b7
Nidheesh Menon <nidheeshsmenon@gmail.com>
2026-07-05 07:06:36 +0530

    Uncomment portfolio calculations and print output

--- projects/Project 2 - Portfolio Analyzer.py
+++ projects/Project 2 - Portfolio Analyzer.py
@@ -8,14 +8,12 @@ portfolio = pd.read_csv("data/portfolio.csv")
 # print(portfolio.info())
 # print(portfolio.isna().sum().sum())
 # print(portfolio.describe())
-# portfolio["Invested Amount"] = (portfolio["Qty"] * portfolio["Avg Price"])
-#
-# portfolio["Current Value"] = (portfolio["Qty"] * portfolio["Current Price"])
-#
-# portfolio["Profit/Loss ₹"] = portfolio["Current Value"] - portfolio["Invested Amount"]
-#
-# portfolio["Profit /Loss %"] = (portfolio["Profit/Loss ₹"]/ portfolio["Invested Amount"]) * 100
-#
-# print(portfolio)
-print(type(portfolio["Qty"]))
-print(type(["Qty"]))
+ portfolio["Invested Amount"] = (portfolio["Qty"] * portfolio["Avg Price"])
+
+ portfolio["Current Value"] = (portfolio["Qty"] * portfolio["Current Price"])
+
+ portfolio["Profit/Loss ₹"] = portfolio["Current Value"] - portfolio["Invested Amount"]
+
+ portfolio["Profit /Loss %"] = (portfolio["Profit/Loss ₹"]/ portfolio["Invested Amount"]) * 100
+
+ print(portfolio)
