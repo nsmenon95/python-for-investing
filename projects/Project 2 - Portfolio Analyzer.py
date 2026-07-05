a8183fd228616dbc5ba010b9bfe265b21c1d2815
nsmenon95 <nidheeshsmenon@gmail.com>
2026-07-05 16:52:34 +0530

    feat: add portfolio summary

--- projects/Project 2 - Portfolio Analyzer.py
+++ projects/Project 2 - Portfolio Analyzer.py
@@ -17,5 +17,24 @@ portfolio = pd.read_csv("data/portfolio.csv")
 # portfolio["Profit /Loss %"] = (portfolio["Profit/Loss ₹"]/ portfolio["Invested Amount"]) * 100
 #
 # print(portfolio)
-print(type(portfolio["Qty"]))
-print(type(["Qty"]))
+print("="*45)
+print("\t\tPORTFOLIO SUMMARY")
+print("="*45)
+print("Total Stocks : ",portfolio.shape[0])
+
+portfolio["Invested Amount"] = (portfolio["Qty"] * portfolio["Avg Price"])
+portfolio["Current Value"] = (portfolio["Qty"] * portfolio["Current Price"])
+portfolio["Profit/Loss ₹"] = portfolio["Current Value"] - portfolio["Invested Amount"]
+portfolio["Profit/Loss %"] = (portfolio["Profit/Loss ₹"]/ portfolio["Invested Amount"]) * 100
+
+total_invested = portfolio["Invested Amount"].sum()
+print("Total Invested : ",total_invested)
+
+total_current = portfolio["Current Value"].sum()
+print("Current Value : ",total_current)
+
+overall_profit = portfolio["Profit/Loss ₹"].sum()
+print("Overall Profit : ",overall_profit)
+
+overall_return = (overall_profit/total_invested)*100
+print("Overall Return : ",overall_return)
