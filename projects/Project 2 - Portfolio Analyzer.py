dabb95fda48e435386b5142cb086d005cbe85480
nsmenon95 <nidheeshsmenon@gmail.com>
2026-07-05 17:28:08 +0530

    feat: identify best and worst performers

--- projects/Project 2 - Portfolio Analyzer.py
+++ projects/Project 2 - Portfolio Analyzer.py
@@ -1,40 +1,59 @@
 import pandas as pd
 
-
+print("="*45)
+print("\t\tLOAD DATA")
+print("="*45)
 portfolio = pd.read_csv("data/portfolio.csv")
 # print(portfolio)
-# print(portfolio.shape)
-# print(portfolio.head())
-# print(portfolio.info())
-# print(portfolio.isna().sum().sum())
-# print(portfolio.describe())
-# portfolio["Invested Amount"] = (portfolio["Qty"] * portfolio["Avg Price"])
-#
-# portfolio["Current Value"] = (portfolio["Qty"] * portfolio["Current Price"])
-#
-# portfolio["Profit/Loss ₹"] = portfolio["Current Value"] - portfolio["Invested Amount"]
-#
-# portfolio["Profit /Loss %"] = (portfolio["Profit/Loss ₹"]/ portfolio["Invested Amount"]) * 100
-#
-# print(portfolio)
-print("="*45)
-print("\t\tPORTFOLIO SUMMARY")
-print("="*45)
-print("Total Stocks : ",portfolio.shape[0])
-
+print(portfolio.shape)
+print(portfolio.head())
+portfolio.info()
+print(portfolio.isna().sum().sum())
+# print("="*45)
+# print("\t\tCALCULATED COLUMNS")
+# print("="*45)
 portfolio["Invested Amount"] = (portfolio["Qty"] * portfolio["Avg Price"])
 portfolio["Current Value"] = (portfolio["Qty"] * portfolio["Current Price"])
 portfolio["Profit/Loss ₹"] = portfolio["Current Value"] - portfolio["Invested Amount"]
 portfolio["Profit/Loss %"] = (portfolio["Profit/Loss ₹"]/ portfolio["Invested Amount"]) * 100
 
+
+print("="*45)
+print("\t\tPORTFOLIO SUMMARY")
+print("="*45)
+
+print("Total Stocks : ",portfolio.shape[0])
+
 total_invested = portfolio["Invested Amount"].sum()
-print("Total Invested : ",total_invested)
+print(f"Total Invested :  ₹ {total_invested:,.2f}")
 
 total_current = portfolio["Current Value"].sum()
-print("Current Value : ",total_current)
+print(f"Current Value : ₹{total_current:,.2f}")
 
 overall_profit = portfolio["Profit/Loss ₹"].sum()
 print("Overall Profit : ",overall_profit)
 
 overall_return = (overall_profit/total_invested)*100
-print("Overall Return : ",overall_return)
+print(f"Overall Return : {overall_return:.2f}%")
+
+print("="*45)
+print("\t\tBEST AND WORST PERFORMER")
+print("="*45)
+best_index = portfolio["Profit/Loss %"].idxmax()
+best = portfolio.loc[best_index]
+worst_index = portfolio["Profit/Loss %"].idxmin()
+worst = portfolio.loc[worst_index]
+
+print("-"*45)
+print("\t\tBEST PERFORMER")
+print("-"*45)
+print("Stock                      : ", best["Stock"])
+print("Return %               : ", best["Profit/Loss %"])
+print("Profit ₹                  : ", best["Profit/Loss ₹"])
+
+print("-"*45)
+print("\t\tWORST PERFORMER")
+print("-"*45)
+print("Stock                      : ", worst["Stock"])
+print("Return %               : ", worst["Profit/Loss %"])
+print("Profit ₹                  : ", worst["Profit/Loss ₹"])
